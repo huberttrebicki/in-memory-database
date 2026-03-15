f2365a8a56bdde43e5f00fd1738658d3d8d850b4
Hubert Trebicki <hubertrebicki@gmail.com>
2026-03-15 11:19:38 +0100

    feat(db): add basic db structure and methods

---
+++ db.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Value struct {
+	Data      []byte
+	ExpiresAt time.Time
+}
+
+type Database struct {
+	Data map[string]Value
+	mut  sync.RWMutex
+	Key  string
+}
+
+var DEFAULT_TTL = 5 * time.Minute
+
+func CreateDatabase() (*Database, error) {
+	godotenv.Load()
+	k := os.Getenv("DB_KEY")
+	if len(k) == 0 {
+		return nil, fmt.Errorf("Database key is not provided")
+	}
+	db := Database{Data: make(map[string]Value), Key: k}
+	return &db, nil
+}
+
+func (db *Database) Authenticate(password string) bool {
+	return password == db.Key
+}
+
+func (db *Database) Set(key string, val []byte) {
+	db.mut.Lock()
+	defer db.mut.Unlock()
+	db.Data[key] = Value{val, time.Now().Add(DEFAULT_TTL)}
+}
+
+func (db *Database) Get(key string) []byte {
+	db.mut.RLock()
+	defer db.mut.Unlock()
+	val, ok := db.Data[key]
+	if !ok || time.Now().After(val.ExpiresAt) {
+		delete(db.Data, key)
+		return nil
+	}
+	val.ExpiresAt = time.Now().Add(DEFAULT_TTL)
+	return val.Data
+}
+
+func (db *Database) Delete(key string) bool {
+	db.mut.Lock()
+	defer db.mut.Unlock()
+	_, ok := db.Data[key]
+	delete(db.Data, key)
+	return ok
+}
